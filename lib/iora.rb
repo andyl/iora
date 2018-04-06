9c66501742c830959b055f2fc05014233ead93cd
andyl <andy@r210.com>
2018-04-05 19:50:11 -0700

    Add guard

--- lib/iora.rb
+++ lib/iora.rb
@@ -1,14 +1,24 @@
+require "forwardable"
 require "iora/version"
 require "repo/github"
 require "repo/test"
 
+# asdf
 class Iora
+
+  extend Forwardable
+
+  attr_accessor :repo, :type
+
+  delegate %i(issue issues) => :repo
+
   def initialize(type, id)
-    case type
-    when :test then Repo::Test.new(id)
-    when :github then Repo::Github.new(id)
-    else
-      raise "Invalid Repo Type"
+    @type = type
+    @repo = case type
+      when :test then Repo::Test.new(id)
+      when :github then Repo::Github.new(id)
+      else
+        raise "Invalid Repo Type"
     end
   end
 end
