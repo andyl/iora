93641fab07cd10ce322d8527bde7b04856d2027b
andyl <andy@r210.com>
2018-04-10 14:45:44 -0700

    Add html_url

--- lib/source/github.rb
+++ lib/source/github.rb
@@ -26,19 +26,25 @@ module Source
     private
 
     def convert(el)
-      base1 = el.to_hash
-        .stringify_keys
+      lcl_fields = Iora::Issue.fields + %w(html_url)
+      sel = el.to_hash.stringify_keys
+      sel
         .map_keys(Iora::Issue.mappings)
         .map_keys({"stm_status" => "state", "exid" => "id"})
-      consolidate_labels(base1)
-        .only(*Iora::Issue.fields)
+        .merge(labels_for(sel))
+        .only(*lcl_fields)
         .without_blanks
     end
 
-    def consolidate_labels(input)
+    def add_fields(input)
       names = input["labels"].map {|el| el[:name]}
       input["stm_labels"] = names.join(", ")
       input
+    end
+
+    def labels_for(el)
+      text = el["labels"].map {|x| x[:name]}.join(", ")
+      { "stm_labels" => text }
     end
 
     def configure_octokit
@@ -51,35 +57,5 @@ module Source
       end
     end
 
-    def sync_bugs
-      issues = Octokit.issues(repo.name)
-      return if issues.blank?
-      issues.each_with_index do |el, idx|
-        attrs = {
-          stm_repo_uuid: repo.uuid         ,
-          type:          "Issue::GitHub"   ,
-          exid:          el["id"]          ,
-          stm_title:     el["title"]       ,
-          stm_labels:    labels_for(el)    ,
-          stm_status:    el["state"]       ,
-          comments:      comments_for(el)  ,
-          html_url:      el["html_url"]    ,
-          synced_at:     BugmTime.now
-        }.stringify_keys
-      end
-    end
-
-    def labels_for(el)
-      el["labels"].map {|x| x[:name]}.join(", ")
-    end
-
-    def comments_for(el)
-      body = Octokit.issue(repo.name, el["number"])[:body]
-      list = Octokit.issue_comments(repo.name, el["number"])
-      lmap = list.blank? ? nil : list.map {|el| el["body"]}.join(" | ")
-      base = [body, lmap].without_blanks.join(" | ")
-      return "" if base.blank? || base.empty?
-      base
-    end
   end
 end
